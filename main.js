f45ca46c7d77ba0598a7cff1a3582cd69fd97d1b
Andrea Caramma <andrea.caramma@soprasteria.com>
2019-10-07 18:08:47 +0200

    aggiunti bottoni e creato div

--- main.js
+++ main.js
@@ -1,97 +1,36 @@
-/*//bottone chiudi e spunta
-var lista_nodi = document.getElementsByTagName("li");
-for (var i = 0; i < lista_nodi.length; i++) {
-  var div_chiudi = document.createElement("div");                     
-  div_chiudi.className = "div_chiudi";                                 
-  document.getElementById("Lista").appendChild(div);                  
-  var button = document.createElement("button");
-  var txt = document.createTextNode("\u00D7");
-  button.className = "Chiudi";
-  button.appendChild(txt);
-  lista_nodi[i].appendChild(button);
-
-  var button2 = document.createElement("button");
-  var txt2 = document.createTextNode("\u2713");
-  button2.className = "Spunta";
-  button2.appendChild(txt2);
-  lista_nodi[i].appendChild(button2);
-}
-
-var chiudi = document.getElementsByClassName("Chiudi");
-for (var i = 0; i < chiudi.length; i++) {
-  chiudi[i].onclick = function() {
-    var div = this.parentElement;
-    div.parentNode.removeChild(div);
-  }
-}
-
-// aggiungi task, bottone e spunta
-function aggiungi() {
-  var li = document.createElement("li");
-  var testo_input = document.getElementById("input").value;
-  var t = document.createTextNode(testo_input);
-  li.appendChild(t);
-
-  if (testo_input === '') {
-    alert("Il campo e' obbligatorio");
-  } else {
-    document.getElementById("Lista").appendChild(li);
-  }
- 	document.getElementById("input").value = "";			
-
-  var button = document.createElement("button");
-  var txt = document.createTextNode("\u00D7");
-  button.className = "Chiudi";
-  button.appendChild(txt);
-  li.appendChild(button);
-
-  var button2 = document.createElement("button");
-  var txt2 = document.createTextNode("\u2713");
-  button2.className = "Spunta";
-  button2.appendChild(txt2);
-  li.appendChild(button2);
-
+/*
+  var chiudi = document.getElementsByClassName("Chiudi");
   for (var i = 0; i < chiudi.length; i++) {
-    chiudi[i].onclick = function() {
-      var div = this.parentElement;
-      div.parentNode.removeChild(div);
-    }
-  }
+  chiudi[i].onclick = function() {
+  var div = this.parentElement;
+  div.parentNode.removeChild(div);
+*/
+function aggiungi() {
+  //crea div task
+  var div = document.createElement("div");
+  div.className = "task";
+  div.innerHTML = document.getElementById("input").value;
+  var container = document.getElementById("container");
+  container.appendChild(div);
 
+  //crea task, bottone chiudi e spunta
+  var testo_input = document.getElementById("input").value;
+  var task = "<span>" + testo_input + "</span>";
+  document.getElementById('input').value = "";
   
-}*/
+  var chiudi_bottone = "<button id='Chiudi' onclick='rimuovi()'>\u00D7</button>";
+  var spunta = "<button id='Spunta'>\u2713</button>";
+  div.innerHTML = task + "<div>" + chiudi_bottone + spunta + "</div>";
+}
 
-
-
- function aggiungi() {
-      //crea div
-      var div = document.createElement('div');
-      div.className = "task";
-      div.innerHTML = document.getElementById('input').textContent;
-      var container = document.getElementById('container');
-      container.appendChild(div);
-
-      //crea task
-      var testo_input = document.getElementById("input").value;
-      div.innerHTML = "<span>" + testo_input + "</span>";   
-    
-    
+function rimuovi(){
 
 }
-   /*
-    if (testo_input === '') {
-      alert("Il campo e' obbligatorio");
-    } else {
-      document.getElementsByClassName("task1").appendChild(theDiv);
-    }
 
-    var button = document.createElement("button");
-    var txt = document.createTextNode("\u00D7");
-    button.className = "Chiudi";
-    button.appendChild(txt);
-
-    var button2 = document.createElement("button");
-    var txt2 = document.createTextNode("\u2713");
-    button2.className = "Spunta";
-    button2.appendChild(txt2);  */
-
+/*
+var divbtn = document.createElement("div");
+  divbtn.className = "btn_div";
+  divbtn.innerHTML = document.getElementById("input").value;
+  var divtask = document.getElementsByClassName("task");
+  divtask.appendChild(divbtn);
+ divbtn.innerHTML = chiudi_bottone + spunta;*/
