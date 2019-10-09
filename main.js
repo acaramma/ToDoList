cd48058358beca6c78c9fe1e20e71be4137e24e9
Andrea Caramma <andrea.caramma@soprasteria.com>
2019-10-09 15:34:50 +0200

    push

--- main.js
+++ main.js
@@ -1,30 +1,38 @@
+let numtask = 0;
 function aggiungi() {
   //crea div task
-  var div = document.createElement("div");
-  div.setAttribute("id", "task");
+
+  let div = document.createElement("div");
+  div.className = "task";
   div.innerHTML = document.getElementById("input").value;
-  var container = document.getElementById("container");
+  let container = document.getElementById("container");
   container.appendChild(div);
 
   //crea task, bottone chiudi e spunta
-  var testo_input = document.getElementById("input").value;
-  document.getElementById('input').value = "";
+  let testo_input = document.getElementById("input").value;
+  document.getElementById("input").value = "";
 
-  var bottoni ="<span id='text'>";
+  let bottoni ="<span class='text'>";
   bottoni += testo_input;
-  bottoni += "</span><div class='button'><button id='Chiudi' onclick='rimuovi()'>&#128465;</button><button id='Spunta' onclick='spuntato()'>\u2713</button></div>"
+  bottoni += "</span><div class='bottone'><button id='Chiudi' onclick='rimuovi()'>&#128465;</button><button id='Spunta' onclick='spuntato()'>\u2713</button></div>";
   div.innerHTML = bottoni;
+  numtask++;
 }
+
 
 function rimuovi(){
-  var canc = document.getElementById("task");
-  canc.parentNode.removeChild(canc);
+  var canc = document.getElementsByClassName("task");
+  var len = canc.length;
+  for(var i = 0; i < len; i++) {
+    canc[i].parentNode.removeChild(canc[i]);
+  }
 }
+      
 
-function spuntato(){  
-  if(document.getElementById("text").style.textDecoration == "line-through"){
-    document.getElementById("text").style.textDecoration = "none";
+function spuntato(){    
+  if(document.getElementsByClassName("text").style.textDecoration == "line-through"){
+    document.getElementsByClassName("text").style.textDecoration = "none";
   }else{
-    document.getElementById("text").style.textDecoration = "line-through";
+    document.getElementsByClassName("text").style.textDecoration = "line-through";
   }
 }
