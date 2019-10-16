ce86eee3160c0f5600fc467ced08dbc6330cec94
Andrea Caramma <andrea.caramma@soprasteria.com>
2019-10-16 16:32:55 +0200

    fixed tasto spunta

--- main.js
+++ main.js
@@ -1,38 +1,36 @@
-let numtask = 0;
 function aggiungi() {
   //crea div task
-
-  let div = document.createElement("div");
+  var div = document.createElement("div");
   div.className = "task";
+  //div.setAttribute("id", "task");
   div.innerHTML = document.getElementById("input").value;
-  let container = document.getElementById("container");
+  var container = document.getElementById("container");
   container.appendChild(div);
 
   //crea task, bottone chiudi e spunta
   let testo_input = document.getElementById("input").value;
   document.getElementById("input").value = "";
 
-  let bottoni ="<span class='text'>";
+  let bottoni ="<span id='text'>";
   bottoni += testo_input;
   bottoni += "</span><div class='bottone'><button id='Chiudi' onclick='rimuovi()'>&#128465;</button><button id='Spunta' onclick='spuntato()'>\u2713</button></div>";
   div.innerHTML = bottoni;
-  numtask++;
 }
 
+function spuntato(){
+  if (document.getElementById("text").style.textDecoration === "line-through"){
+    document.getElementById("text").style.textDecoration = "none";
+  }else{
+    document.getElementById("text").style.textDecoration = "line-through";
+  }
+}
 
 function rimuovi(){
   var canc = document.getElementsByClassName("task");
-  var len = canc.length;
-  for(var i = 0; i < len; i++) {
-    canc[i].parentNode.removeChild(canc[i]);
-  }
-}
-      
-
-function spuntato(){    
-  if(document.getElementsByClassName("text").style.textDecoration == "line-through"){
-    document.getElementsByClassName("text").style.textDecoration = "none";
-  }else{
-    document.getElementsByClassName("text").style.textDecoration = "line-through";
+  var button = document.getElementById("Chiudi");
+  button.onclick = function(){
+    for (var i = 0; i < canc.length; i++) {
+      canc[i].parentNode.removeChild(canc[i]);
+    }
   }
 }
